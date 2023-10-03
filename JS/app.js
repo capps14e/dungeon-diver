e981255ee6b60db076cfa77c0795f3ea3d909388
Tom <capps14e@gmail.com>
2023-10-03 12:22:33 -0400

    added scripts for dialogue

--- JS/app.js
+++ JS/app.js
@@ -3,47 +3,73 @@ const optionButtonsElement = document.getElementById('option-buttons')
 
 let state = {}
 
-
 function startGame() {
-
-
+    state = {}
+    showTextNode(1)
 }
 
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
 
-function showTextNode(TextNodeIndex) {
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
+}
 
-
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
 }
 
 function selectOption(option) {
-    
-
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0) {
+        return startGame()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
 }
 
 const textNodes = [
 
-{
-    id: 1, 
-    text: 'You stand at the entrance of a crypt with sword in hand.'
+    {
+        id: 1,
+        text: 'You stand at the entrance of a crypt with sword in hand.'
     options: [
 
-        {
-            text: 'Enter',
-            setState: { enterNow: true}
+            {
+                text: 'Enter',
+                setState: { enterNow: true }
             nextText: 2
-        },
-        {
-            text: 'Flee!'
-            next text: 2
+            },
+            {
+                text: 'Flee!'
+            nextText: 2
 
-        }
-    ]
-},
-{
-    id: 2
-}
+            }
+        ]
+    },
+    {
+        id: 2
+        text: 'You enter through a dim hallway. Cobwebs pull at your face. Decay fills your nose. Your path forks left and right.'
+        options: [
+            {
+                text: 'Move Forward'
+            text: 'Move left'
+            text: 'Move right'
 
-
+            }
+        ]
+    }
 ]
 
 startGame()
