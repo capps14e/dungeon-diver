e2e26046451aed005290b8e8c9e92412c992b8d5
Tom <capps14e@gmail.com>
2023-10-03 11:50:36 -0400

    added scripts and styles

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,49 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
 
+let state = {}
+
+
+function startGame() {
+
+
+}
+
+
+function showTextNode(TextNodeIndex) {
+
+
+}
+
+function selectOption(option) {
+    
+
+}
+
+const textNodes = [
+
+{
+    id: 1, 
+    text: 'You stand at the entrance of a crypt with sword in hand.'
+    options: [
+
+        {
+            text: 'Enter',
+            setState: { enterNow: true}
+            nextText: 2
+        },
+        {
+            text: 'Flee!'
+            next text: 2
+
+        }
+    ]
+},
+{
+    id: 2
+}
+
+
+]
+
+startGame()
